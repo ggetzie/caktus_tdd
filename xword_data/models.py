4df0f0ae7c5a0bbdf28cb432ae6f79a527363188
gabe <ggetzie@gmail.com>
2020-09-09 18:35:12 +0800

    xword_data models created

--- xword_data/models.py
+++ xword_data/models.py
@@ -1,3 +1,49 @@
 from django.db import models
 
-# Create your models here.
+class Puzzle(models.Model):
+    title = models.CharField("Title",
+                             default="",
+                             max_length=255,
+                             blank=True)
+    date = models.DateField("Publication Date")
+    byline = models.CharField("Byline", max_length=255)
+    publisher = models.CharField("Publisher", max_length=12)
+
+    class Meta:
+        db_table = "puzzles"
+
+    def __str__(self):
+        return f"{self.date} - {self.publisher}"
+
+    
+class Entry(models.Model):
+    entry_text = models.CharField("Entry Text",
+                                  max_length=50,
+                                  unique=True)
+
+    class Meta:
+        db_table = "entries"
+        verbose_name_plural = "Entries"
+
+    def __str__(self):
+        return f"{self.entry_text}"
+
+
+class Clue(models.Model):
+    entry = models.ForeignKey(Entry,
+                              on_delete=models.SET_NULL,
+                              null=True)
+    puzzle = models.ForeignKey(Puzzle,
+                               on_delete=models.SET_NULL,
+                               null=True)
+    clue_text = models.CharField("Clue Text",
+                                 max_length=512)
+    theme = models.BooleanField("Theme", default=False)
+
+    class Meta:
+        db_table = "clues"
+
+    def __str__(self):
+        return f"{self.clue_text}"
+                                 
+    
